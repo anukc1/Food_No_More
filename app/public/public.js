3ed079553983c41af709349fb06dfd81e82c199f
Anu KC <anukc2000@gmail.com>
2019-07-25 12:55:57 -0500

    vue added

---
+++ app/public/public.js
@@ -0,0 +1,34 @@
+// $("#signupSubmit").on("click", function(){
+
+//     var userData = {
+       
+//         firstname: $("#firstName").val().trim(),
+//         lastname: $("#LastName").val().trim(),
+//         email: $("#email").val().trim(),
+//         password: $("signUpPassword")
+//     };
+
+//     console.log(userData);
+
+//     // send the user sign up information to back end.
+//     $.post("/api/", userData).then(function(data){
+
+        
+//     }) 
+
+
+
+// })
+
+
+
+
+// vue part---new technology-----
+    
+    var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
+  
