2e7e3a04e83f3e9771e8b06aac7009cebc113674
Anu KC <anukc2000@gmail.com>
2019-07-29 10:54:23 -0500

    select food button working

--- app/public/calories.js
+++ app/public/calories.js
@@ -1,25 +1,10 @@
-
 $("#submitFoodSearch").on('click', function(event){
     event.preventDefault();
   var food = $('#fname').val().trim();
     var id = $('#fname').data('userid');
-
     localStorage.setItem('userId', id)
-
     window.location.replace("/searchfood/" + food )
 });
 
-$("#buttonAddFood").click(function(event) {
-    event.preventDefault()
-    console.log('hello');
-});
 
-// $("#buttonAddFood").on("click", function(event){
-//     console.log('hello');
-//     // event.preventDefault();
-//     // var foodId = $(this).data('foodid');
-//     // var userId = localStorage.getItem('userId');
 
-//     // console.log('food id: ' + foodId + "\n" + "user id: " + userId)
-
-// })
